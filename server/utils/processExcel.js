dcbb50427cbe8c516e4fe723124773d2ecb7dacf
imkhateeb <luckykhateeb2@gmail.com>
2024-11-21 22:04:42 +0530

    [Bug]: Backend issue resolved

--- server/utils/processExcel.js
+++ server/utils/processExcel.js
@@ -1,6 +1,4 @@
 const xlsx = require("xlsx");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY);
 
 const processExcel = async (filePath) => {
   const workbook = xlsx.readFile(filePath);
--- server/utils/processPdf.js
+++ server/utils/processPdf.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 const fs = require("fs");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY);
 const pdfParse = require("pdf-parse");
 
 const processPdf = async (filePath) => {
